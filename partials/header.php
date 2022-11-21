94ef92768fef0cadc97e5228730255874167fb3a
FougnigueOuattara <Karimouattara122@gmail.com>
2022-11-21 12:14:04 +0000

    first commit

---
+++ partials/header.php
@@ -0,0 +1,10 @@
+<header class="header-menu">
+    <div><a href="#"><img src="../images/E-menage1.png" alt="logo" class="logo"></a></div>
+    <nav class="nav-menu">
+        <ul class="menu-items">
+            <li><a href="#">Cuisiniers</a></li>
+            <li><a href="#">Menageres</a></li>
+            <li><a href="#">Employeurs</a></li>
+        </ul>
+    </nav>
+</header>
